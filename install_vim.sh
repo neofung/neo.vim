a645020e63e846fcfe880c4f7648648b23499cad
neofung@lab <neosfung@gmail.com>
2013-05-08 20:00:58 +0800

    Fixed the bug of compiling VIM

--- install_vim.sh
+++ install_vim.sh
@@ -6,7 +6,7 @@ sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
     libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev ruby-dev mercurial 
 sudo apt-get remove vim vim-runtime gvim vim-tiny vim-common
 
-hg clone https://code.google.com/p/vim/
+#hg clone https://code.google.com/p/vim/
 cd vim
 make uninstall
 make distclean
@@ -16,8 +16,9 @@ make distclean
             --enable-perlinterp \
             --enable-gui=gnome2 \
             --enable-cscope \
-            --prefix=/usr/local/vim \
-            --with-python-config-dir=/usr/lib/python2.7/config/
-make VIMRUNTIMEDIR=/usr/share/vim/vim73
+            --prefix=/usr/local \
+            --with-python-config-dir=/usr/lib/python2.7/config/ \
+	    --with-compileby="neofung<neo.s.fung.gmail.com>"
+make
 sudo make install
 
