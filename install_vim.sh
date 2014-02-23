f0724c03713c15a7dfc8c6144da60bf08fe41b32
neofung <neosfung@gmail.com>
2014-02-23 16:24:06 +0800

    Update install_vim.sh
    install libncurses5-dev, to fix "tgetent()... configure error not found" error

--- install_vim.sh
+++ install_vim.sh
@@ -1,6 +1,6 @@
 sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
     libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
-    libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev ruby-dev mercurial 
+    libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev ruby-dev mercurial libncurses5-dev
 sudo apt-get remove vim vim-runtime gvim vim-tiny vim-common
 
 mkdir ~/temp
