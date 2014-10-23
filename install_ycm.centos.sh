ab8f4d25c467a8ad3986ebc20e2b7fd52abc5446
Neo Fung <neosfung@gmail.com>
2014-10-23 23:15:57 +0800

    Add install_ycm.centos.sh

---
+++ install_ycm.centos.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#########################################################################
+# Author: neofung(http://blog.csdn.net/neofung)
+# Created Time: 2014年05月18日 星期日 16时52分30秒
+# File Name: install_ycm.sh
+# Description: 
+#########################################################################
+
+sudo yum install build-essential cmake python-deve
+
+cd ~/.vim/bundle/YouCompleteMe
+git submodule update --init --recursive
+
+mkdir ~/temp
+cd ~/temp
+
+mkdir ycm_build
+cd ycm_build
+make clean
+cmake -G "Unix Makefiles" \
+    -DUSE_SYSTEM_LIBCLANG=ON \
+    . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+make ycm_core
+make ycm_support_libs
+make
